51cbe8a4d2dc06464a39f2b1d6b6aab45022f6ac
Kelwin Jhackson <141432193+Kelwinkxps13@users.noreply.github.com>
2024-11-26 15:19:51 -0300

    Add files via upload

--- avaliacoes/avaliacao-04/18-sqlite3-02/18-sqlite3-02.dart
+++ avaliacoes/avaliacao-04/18-sqlite3-02/18-sqlite3-02.dart
@@ -5,11 +5,11 @@ import 'package:sqflite/sqflite.dart';
 import 'package:path/path.dart';
 
 class Aluno {
-  int id = 1;
-  String nome = 'Kelwin Jhackson';
-  String dataNascimento = '03/08/2006';
+  int id;
+  String nome;
+  String dataNascimento;
 
-  Aluno({required this.id,required this.nome,required this.dataNascimento});
+  Aluno({required this.id, required this.nome, required this.dataNascimento});
 
   Map<String, dynamic> toMap() {
     return {
@@ -23,15 +23,15 @@ class Aluno {
 class AlunoDatabase {
   static final AlunoDatabase instance = AlunoDatabase._init();
 
-  static Database _database;
+  static Database? _database;
 
   AlunoDatabase._init();
 
   Future<Database> get database async {
-    if (_database != null) return _database;
+    if (_database != null) return database;
 
     _database = await _initDB('aluno.db');
-    return _database;
+    return database;
   }
 
   Future<Database> _initDB(String dbName) async {
@@ -115,21 +115,38 @@ void main() async {
   final db = AlunoDatabase.instance;
 
   // Inserir um aluno
-  final aluno = Aluno(nome: 'João', dataNascimento: '2000-01-01');
-  int alunoId = await db.insertAluno(aluno);
+  final aluno1 =
+      (Aluno(id: 1, nome: 'Kelwin Jhackson', dataNascimento: '03/08/2006'));
+  final aluno2 =
+      (Aluno(id: 2, nome: 'Lucas Costa', dataNascimento: '01/12/2007'));
+  final aluno3 =
+      (Aluno(id: 3, nome: 'Vinicius Mango', dataNascimento: '14/06/2007'));
+  int aluno1x = await db.insertAluno(aluno1);
+  int aluno2x = await db.insertAluno(aluno2);
+  int aluno3x = await db.insertAluno(aluno3);
 
   // Buscar um aluno pelo ID
-  Aluno retrievedAluno = await db.getAluno(alunoId);
-  print('Aluno recuperado: $retrievedAluno');
+  Aluno retrievedAluno1 = await db.getAluno(aluno1x);
+  Aluno retrievedAluno2 = await db.getAluno(aluno2x);
+  Aluno retrievedAluno3 = await db.getAluno(aluno3x);
+  print('Aluno recuperado: $retrievedAluno1');
+  print('Aluno recuperado: $retrievedAluno2');
+  print('Aluno recuperado: $retrievedAluno3');
 
   // Atualizar os dados de um aluno
-  retrievedAluno.nome = 'João da Silva';
-  await db.updateAluno(retrievedAluno);
+  retrievedAluno1.nome = 'Kelwin Jhackson';
+  retrievedAluno2.nome = 'Lucas Costa';
+  retrievedAluno3.nome = 'Vinicius Mango';
+  await db.updateAluno(retrievedAluno1);
+  await db.updateAluno(retrievedAluno2);
+  await db.updateAluno(retrievedAluno3);
 
   // Buscar todos os alunos
   List<Aluno> alunos = await db.getAllAlunos();
   print('Todos os alunos: $alunos');
 
   // Deletar um aluno
-  await db.deleteAluno(alunoId);
+  await db.deleteAluno(aluno1x);
+  await db.deleteAluno(aluno2x);
+  await db.deleteAluno(aluno3x);
 }
